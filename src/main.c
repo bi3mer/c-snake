c723bed10c370f52f06936eae7e14d3a44b502c4
bi3mer <colan@comcast.net>
2025-11-14 13:22:05 -0500

    scene and menu implented

--- src/main.c
+++ src/main.c
@@ -1,19 +1,90 @@
 #include "raylib.h"
 
+typedef enum
+{
+    Scene_Menu = 0,
+    Scene_Game,
+    Scene_Error
+} Scene;
+
 int main(void)
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screen_width = 800;
+    const int screen_height = 800;
 
-    InitWindow(screenWidth, screenHeight, "Raylib Template");
+    unsigned int frame_counter = 0;
+    Scene scene = Scene_Menu;
+
+    InitWindow(screen_width, screen_height, "Snake");
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ///////////////////////////////////////////////////////////////////////
+        // Game Logic
+        ++frame_counter;
 
-        DrawText("TEXT HERE", 190, 200, 20, BLACK);
+        switch (scene)
+        {
+        case Scene_Menu:
+        {
+            if (IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_ENTER))
+            {
+                scene = Scene_Game;
+            }
+
+            break;
+        }
+        case Scene_Game:
+        {
+            if (IsKeyPressed(KEY_Q))
+            {
+                scene = Scene_Menu;
+            }
+
+            break;
+        }
+        case Scene_Error:
+        default:
+            break;
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+        // Render Logic
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        switch (scene)
+        {
+        case Scene_Menu:
+        {
+            const char *title = "Snake";
+            const int title_font_size = 50;
+            const int title_width = MeasureText(title, title_font_size);
+
+            DrawText(title, (screen_width - title_width) / 2, screen_height / 4,
+                     title_font_size, WHITE);
+
+            if ((frame_counter / 30) % 2)
+            {
+                const char *instructions = "Press 'space' to Play!";
+                const int i_font_size = 30;
+                const int i_width = MeasureText(instructions, i_font_size);
+
+                DrawText(instructions, (screen_width - i_width) / 2,
+                         screen_height / 2, i_font_size, WHITE);
+            }
+            break;
+        }
+        case Scene_Game:
+        {
+            DrawText("GAME", 190, 200, 20, WHITE);
+            break;
+        }
+        case Scene_Error:
+        default:
+            break;
+        }
 
         EndDrawing();
     }
