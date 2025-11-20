c97c465bcdce0ded744e5a74f12b11279609165d
bi3mer <colan@comcast.net>
2025-11-20 11:51:57 -0500

    scenes

--- src/main.c
+++ src/main.c
@@ -1,19 +1,69 @@
 #include "raylib.h"
 
+typedef enum
+{
+    Scene_Menu = 0,
+    Scene_Game
+} Scene;
+
 int main(void)
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screen_width = 800;
+    const int screen_height = 800;
 
-    InitWindow(screenWidth, screenHeight, "Raylib Template");
+    Scene current_scene = Scene_Menu;
+
+    InitWindow(screen_width, screen_height, "Snake");
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
 
-        DrawText("TEXT HERE", 190, 200, 20, BLACK);
+        ///////////////////////////////////////////////////////////////////////
+        /// Update the Game
+        switch (current_scene)
+        {
+        case Scene_Menu:
+        {
+            if (IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_ENTER))
+            {
+                current_scene = Scene_Game;
+            }
+            break;
+        }
+        case Scene_Game:
+        {
+            if (IsKeyPressed(KEY_Q))
+            {
+                current_scene = Scene_Menu;
+            }
+
+            break;
+        }
+        default:
+            break;
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+        /// Render the Game
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        switch (current_scene)
+        {
+        case Scene_Menu:
+        {
+            DrawText("Menu Scene", 450, 450, 40, WHITE);
+            break;
+        }
+        case Scene_Game:
+        {
+            DrawText("Game Scene", 450, 450, 40, WHITE);
+            break;
+        }
+        default:
+            break;
+        }
 
         EndDrawing();
     }
