a500d28df2aab586e3d19336e9b7187f83ce239b
bi3mer <colan@comcast.net>
2025-11-20 12:01:23 -0500

    menu scene implemented

--- src/main.c
+++ src/main.c
@@ -11,6 +11,10 @@ int main(void)
     const int screen_width = 800;
     const int screen_height = 800;
 
+    unsigned int frame_count = 0;
+
+    bool show_menu_instructions = true;
+
     Scene current_scene = Scene_Menu;
 
     InitWindow(screen_width, screen_height, "Snake");
@@ -18,6 +22,7 @@ int main(void)
 
     while (!WindowShouldClose())
     {
+        ++frame_count;
 
         ///////////////////////////////////////////////////////////////////////
         /// Update the Game
@@ -28,6 +33,11 @@ int main(void)
             if (IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_ENTER))
             {
                 current_scene = Scene_Game;
+            }
+
+            if (frame_count % 30 == 0)
+            {
+                show_menu_instructions = !show_menu_instructions;
             }
             break;
         }
@@ -53,7 +63,23 @@ int main(void)
         {
         case Scene_Menu:
         {
-            DrawText("Menu Scene", 450, 450, 40, WHITE);
+            const char *title = "Snake";
+            const int t_size = 40;
+            const int t_w = MeasureText(title, t_size);
+
+            DrawText(title, (screen_width - t_w) / 2, screen_height / 4, t_size,
+                     WHITE);
+
+            if (show_menu_instructions)
+            {
+                const char *instructions = "Press 'space' to Play";
+                const int i_size = 30;
+                const int i_w = MeasureText(instructions, i_size);
+
+                DrawText(instructions, (screen_width - i_w) / 2,
+                         screen_height / 2, i_size, WHITE);
+            }
+
             break;
         }
         case Scene_Game:
